059b7bd0333c6c53cc41b339a419eec836be60b8
SHAIK SHER ALI <shaiksherali2003@gmail.com>
2024-06-01 15:57:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todos_list1/',
   plugins: [react()],
-  base: '/todos_list1/', // Replace 'your-repo-name' with your GitHub repository name
-  build: {
-    outDir: 'dist', // This is the default, but ensure it is set correctly
-  },
 })
